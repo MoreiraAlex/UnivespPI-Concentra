f13ad0a1437a565a8ac94d24aaff28e066885c0e
Alex Moreira <celerom100@gmail.com>
2021-11-24 02:26:47 -0300

    Configurando Navbar

--- concentra/views.py
+++ concentra/views.py
@@ -5,7 +5,6 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
-@login_required
 def consulta(request):    
     if request.method == 'POST':
         form = ConsultaForm(request.POST)
